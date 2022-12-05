186d7b4075a9c414a6b9762b61836720319b6dab
AderBarbosa <>
2022-12-05 23:00:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  Int16 num;
+  String entrada;
+  Console.WriteLine("Digite o número");
+  entrada = Console.ReadLine();
+  num = Int16.Parse(entrada); 
+  if (num%2==0)
+  {  
+  Console.WriteLine ("Número par");
+  } 
+  else {
+  Console.WriteLine("Número Ímpar");
+  }
+}
+}
